de74cf31d14069d9af7e097eeb8dd5d0068163fb
Duke <jd.daniel.contact@gmail.com>
2025-01-16 00:01:58 -0600

    Add code for scatter graph using seaborn

--- Seaborn/Examples/1.py
+++ Seaborn/Examples/1.py
@@ -1,3 +1,13 @@
 import seaborn as sns
 
 propinas = sns.load_dataset('tips')
+
+sns.relplot(
+    data = propinas,
+    x = 'Total bill',
+    y = 'Tip',
+    hue = 'Sex',
+    style = 'Smoker',
+    size = 'Size'
+);
+# Seaborn se encarga de crear el grafico mas adeciado para mostrar la informacion
